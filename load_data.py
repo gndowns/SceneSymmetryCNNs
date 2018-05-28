e285ca6472b30045c7caa3c4c98f02e19eafa0f4
Gabriel Downs <gndowns@gmail.com>
2018-05-28 11:45:12 -0400

    use data augmentation for mit67 rgb

--- load_data.py
+++ load_data.py
@@ -123,17 +123,24 @@ def mit67_rgb():
   train_dir = 'data/mit67/rgb/train'
   test_dir = 'data/mit67/rgb/test'
   
-  # no augmentation required, plenty of data present
-  datagen = ImageDataGenerator(rescale = 1. / 255)
+  # Data Augment
+  train_datagen = ImageDataGenerator(
+    rescale = 1. / 255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True
+  )
+  # only rescale for testing
+  test_datagen = ImageDataGenerator(rescale = 1. / 255)
 
-  train_gen = datagen.flow_from_directory(
+  train_gen = train_datagen.flow_from_directory(
     train_dir,
     target_size = (img_width, img_height),
     batch_size = batch_size,
     class_mode = 'categorical'
   )
 
-  test_gen = datagen.flow_from_directory(
+  test_gen = test_datagen.flow_from_directory(
     test_dir,
     target_size = (img_width, img_height),
     batch_size = batch_size,
