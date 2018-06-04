9f1a44d4a9fb11cd7ea4a17fffdd10a6eb099d86
Gabriel Downs <gndowns@gmail.com>
2018-06-04 11:28:48 -0400

    checkpoint for saving only the best weights

--- top_conv_block.py
+++ top_conv_block.py
@@ -10,11 +10,44 @@ from keras.models import Sequential, Model
 from keras.layers import Input, Flatten, Dense, Dropout
 from keras.optimizers import SGD, Adam
 from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
 
 #  EPOCHS = 50
 # seems to plateau after this
-EPOCHS = 25
+#  EPOCHS = 25
+# run for long time and save best weights using Checkpoints
+EPOCHS = 100
 
+
+# Convert single channel B/W images to 3 channels (all equal)
+def to_rgb(input_shape, train_dir, test_dir, img_width, img_height, batch_size):
+  # augment to 3 channels
+  input_shape = (input_shape[0], input_shape[1], 3)
+
+  # data generators
+  train_datagen = ImageDataGenerator(
+    rescale = 1. / 255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True
+  )
+  test_datagen = ImageDataGenerator(rescale = 1. / 255)
+  
+  # generators, leave out 'grayscale' color_mode option
+  train_gen = train_datagen.flow_from_directory(
+    train_dir,
+    target_size = (img_width, img_height),
+    batch_size = batch_size,
+    class_mode = 'categorical'
+  )
+  test_gen = test_datagen.flow_from_directory(
+    test_dir,
+    target_size = (img_width, img_height),
+    batch_size = batch_size,
+    class_mode = 'categorical'
+  )
+
+  return (input_shape, train_gen, test_gen)
 
 
 # Link VGG16 base with our trained top_model classifier
@@ -54,7 +87,7 @@ def compile_model(input_shape, nb_classes, dataset_str):
 
   return model
 
-def train(model, train_gen, test_gen, batch_size, nb_train_samples, nb_test_samples):
+def train(model, dataset_str, train_gen, test_gen, batch_size, nb_train_samples, nb_test_samples):
   print('initial evaluaton: ')
   score = model.evaluate_generator(
     generator = test_gen,
@@ -62,13 +95,32 @@ def train(model, train_gen, test_gen, batch_size, nb_train_samples, nb_test_samp
   )
   print(score)
 
+  # file to save weights to after checkpoints 
+  filepath = dataset_str + '_top_conv_block_weights.h5'
+  # save weights only when performance improves
+  checkpoint = ModelCheckpoint(
+    filepath=filepath,
+    # monitor performance by loss
+    monitor='val_loss',
+    verbose=1,
+    # only save on improvement
+    save_best_only=True,
+    # minimize loss
+    mode='min',
+    # only save weights, not full model
+    save_weights_only=True
+  )
+
+
   print('fine tuning...')
   model.fit_generator(
     train_gen,
     steps_per_epoch=nb_train_samples // batch_size,
     epochs= EPOCHS,
     validation_data=test_gen,
-    validation_steps= nb_test_samples // batch_size
+    validation_steps= nb_test_samples // batch_size,
+    # save weights on improved performance
+    callbacks=[checkpoint]
   )
 
   score = model.evaluate_generator(
@@ -85,28 +137,35 @@ def main():
     'toronto_rgb': load_data.toronto_rgb,
     'toronto_line_drawings': load_data.toronto_line_drawings,
     'mit67_rgb': load_data.mit67_rgb,
-    'mit67_edges': load_data.mit67_edges
+    'mit67_edges': load_data.mit67_edges,
+    'mit67_line_drawings': load_data.mit67_line_drawings
   }
   # SELECT DATASET HERE
-  #  dataset_str = 'mit67_rgb'
-  dataset_str = 'mit67_edges'
   #  dataset_str = 'toronto_rgb'
   #  dataset_str = 'toronto_line_drawings'
+  #  dataset_str = 'mit67_rgb'
+  #  dataset_str = 'mit67_edges'
+  dataset_str = 'mit67_line_drawings'
 
   dataset = datasets[dataset_str]
 
   print('Using dataset ' + dataset_str)
 
   # import data
-  nb_classes, nb_train_samples, nb_test_samples, img_width, img_heigt, \
+  nb_classes, nb_train_samples, nb_test_samples, img_width, img_height, \
     input_shape, batch_size, train_dir, test_dir, train_gen, test_gen = dataset()
+
+  # for black/white datasets, need to convert to 3 channels for VGG
+  if input_shape[2] == 1:
+    input_shape, train_gen, test_gen = to_rgb(input_shape, train_dir, test_dir,
+      img_width, img_height, batch_size)
 
   # compile full VGG + top model
   full_model = compile_model(input_shape, nb_classes, dataset_str)  
   
   print('model compiled')
   # train top conv block and dense layers on selected dataset
-  train(full_model, train_gen, test_gen, batch_size, nb_train_samples, nb_test_samples) 
+  train(full_model, dataset_str, train_gen, test_gen, batch_size, nb_train_samples, nb_test_samples) 
 
   print('saving model...')
   full_model.save(dataset_str + '_top_conv_block.h5')
