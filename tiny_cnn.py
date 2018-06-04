e077aabb8ad328b077f397e14c63f04751a47fdd
Gabriel Downs <gndowns@gmail.com>
2018-06-04 12:16:22 -0400

    add fixed training setup to tiny_cnn

--- tiny_cnn.py
+++ tiny_cnn.py
@@ -2,6 +2,45 @@
 from load_data import toronto_rgb, toronto_line_drawings
 from load_models import mnist, cat_dog
 
+from keras.preprocessing.image import ImageDataGenerator
+
+
+def train(model, train_dir, test_dir, nb_train_samples, nb_test_samples):
+  # Generator for Data augmentation
+  train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+  )
+  # test generator
+  test_datagen = ImageDataGenerator(rescale=1./255)
+
+  train_gen = train_datagen.flow_from_directory(
+    train_dir,
+    # TEMP: fixed
+    target_size=(256, 256),
+    batch_size=16,
+    class_mode='categorical'
+  )
+
+  test_gen = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(256, 256),
+    batch_size=16,
+    class_mode='categorical'
+  )
+
+  model.fit_generator(
+    train_gen,
+    steps_per_epoch = nb_train_samples // 16,
+    epochs = 10,
+    validation_data = test_gen,
+    validation_steps = nb_test_samples // 16
+  )
+
+
+
 def main():
   # Choose and load dataset from options
   datasets = {
@@ -24,5 +63,7 @@ def main():
   # laod and compile model
   model = model(input_shape, nb_classes)
 
+  # train model 
+  train(model, train_dir, test_dir, nb_train_samples, nb_test_samples)
 
 main()
