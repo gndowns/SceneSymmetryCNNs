5c150deee0e43f806cf46fa7c157f259a0f9160e
Gabriel Downs <gndowns@gmail.com>
2018-06-11 15:12:15 -0400

    set colormode from model requirements

--- test.py
+++ test.py
@@ -10,8 +10,8 @@ BATCH_SIZE=16
 
 # Evaluate model on test set of given dataset object
 def evaluate(model, dataset):
-  # grayscale or rgb
-  color_mode = 'rgb' if dataset.nb_channels==3 else 'grayscale'
+  # grayscale or rgb, based on requirements of model
+  color_mode = 'rgb' if model.input_shape[3]==3 else 'grayscale'
 
   test_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -37,7 +37,8 @@ def main():
   #  dataset_str = 'toronto_arc_length_symmetric'
   dataset_str = 'toronto_arc_length_asymmetric'
   # h5 file of saved model
-  model_file = 'toronto_line_drawings_tiny_cnn.h5'
+  #  model_file = 'toronto_line_drawings_tiny_cnn.h5'
+  model_file = 'toronto_line_drawings_top_conv_block.h5'
 
   # load dataset
   print('loading dataset...')
