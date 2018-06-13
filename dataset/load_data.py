81046caea048b597a68618907c5949dc93342e4c
Gabriel Downs <gndowns@gmail.com>
2018-06-13 13:35:02 -0400

    no generators in load_data--- all dataset class based

--- dataset/load_data.py
+++ dataset/load_data.py
@@ -14,47 +14,12 @@ def toronto_rgb():
   nb_train_samples = 60 + 59 + 60 + 60 + 57 + 60
   nb_test_samples = 20 + 20 + 20 + 20 + 19 + 20
 
-  # Standard ImageNet / VGG16 image input size
-  img_width, img_height = 256, 256
   # 3 input channels for R-G-B
   nb_channels = 3
-  input_shape = (img_width, img_height, 3)
-
-  # Chosen as highest w/o out of memory failure
-  batch_size = 16
 
   # directories for test and train data, each contains 1 sub-directory per category
   train_dir = 'data/toronto/rgb/train'
   test_dir = 'data/toronto/rgb/test'
-
-  # Augment training data
-  train_datagen = ImageDataGenerator(
-    # Rescale 0-255 images to 0-1 range
-    rescale = 1. / 255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    horizontal_flip = True
-  )
-
-  # For test data, only rescale
-  test_datagen = ImageDataGenerator(rescale = 1. / 255)
-
-  # Pull images from class sub-directories
-  train_gen = train_datagen.flow_from_directory(
-    train_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    # one class for each sub-directory/category
-    class_mode = 'categorical'
-  )
-  
-  # same for test images
-  test_gen = test_datagen.flow_from_directory(
-    test_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    class_mode = 'categorical'
-  )
 
   return (nb_classes, nb_train_samples, nb_test_samples, nb_channels, train_dir, test_dir)
 
@@ -67,38 +32,10 @@ def toronto_line_drawings():
   nb_test_samples = 20 + 20 + 20 + 20 + 19 + 20
   img_width, img_height = 256,256
   # single input channel for grayscale
-  input_shape = (img_width, img_height, 1)
   nb_channels = 1
-
-  batch_size = 16
 
   train_dir = 'data/toronto/line_drawings/train'
   test_dir = 'data/toronto/line_drawings/test'
-
-  train_datagen = ImageDataGenerator(
-    rescale = 1. / 255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    horizontal_flip = True
-  )
-  test_datagen = ImageDataGenerator(rescale = 1. / 255)
-
-  train_gen = train_datagen.flow_from_directory(
-    train_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    # uses 1 channel instead of 3 (linedrawings are grayscale)
-    color_mode = 'grayscale',
-    class_mode = 'categorical'
-  ) 
-
-  test_gen = test_datagen.flow_from_directory(
-    test_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    color_mode = 'grayscale',
-    class_mode = 'categorical'
-  )
 
   return (nb_classes, nb_train_samples, nb_test_samples, nb_channels, train_dir, test_dir)
 
@@ -179,38 +116,10 @@ def mit67_rgb():
   
   # rgb, 3 channels
   nb_channels = 3
-  input_shape = (img_width, img_height, 3)
-
-  # arbitrary
-  batch_size = 16
   
   train_dir = 'data/mit67/rgb/train'
   test_dir = 'data/mit67/rgb/test'
   
-  # Data Augment
-  train_datagen = ImageDataGenerator(
-    rescale = 1. / 255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    horizontal_flip = True
-  )
-  # only rescale for testing
-  test_datagen = ImageDataGenerator(rescale = 1. / 255)
-
-  train_gen = train_datagen.flow_from_directory(
-    train_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    class_mode = 'categorical'
-  )
-
-  test_gen = test_datagen.flow_from_directory(
-    test_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    class_mode = 'categorical'
-  )
-
   return (nb_classes, nb_train_samples, nb_test_samples, nb_channels, train_dir, test_dir)
 
 
@@ -229,40 +138,10 @@ def mit67_edges():
   
   # line drawings are single channle, B/W
   nb_channels = 1
-  input_shape = (img_width, img_height, 1)
-
-  # arbitrary
-  batch_size = 16 
   
   train_dir = 'data/mit67/edges/train'
   test_dir = 'data/mit67/edges/test'
   
-  # Data Augment
-  train_datagen = ImageDataGenerator(
-    rescale = 1. / 255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    horizontal_flip = True
-  )
-  # only rescale for testing
-  test_datagen = ImageDataGenerator(rescale = 1. / 255)
-
-  train_gen = train_datagen.flow_from_directory(
-    train_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    color_mode = 'grayscale',
-    class_mode = 'categorical'
-  )
-
-  test_gen = test_datagen.flow_from_directory(
-    test_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    color_mode = 'grayscale',
-    class_mode = 'categorical'
-  )
-
   return (nb_classes, nb_train_samples, nb_test_samples, nb_channels, train_dir, test_dir)
 
 
@@ -271,49 +150,14 @@ def mit67_line_drawings():
   # 67 scene categories
   nb_classes = 67
   
-  # These numbers aren't well rounded b/c some image names
-  # are repeated in trainImages.txt & testImages.txt
   nb_train_samples = 5354
   nb_test_samples = 1339
 
-  # just use VGG sizes
-  img_width, img_height = 256, 256
-  
   # line drawings are single channle, B/W
   nb_channels = 1
-  input_shape = (img_width, img_height, 1)
 
-  # arbitrary
-  batch_size = 16 
-  
   train_dir = 'data/mit67/line_drawings/train'
   test_dir = 'data/mit67/line_drawings/test'
   
-  # Data Augment
-  train_datagen = ImageDataGenerator(
-    rescale = 1. / 255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    horizontal_flip = True
-  )
-  # only rescale for testing
-  test_datagen = ImageDataGenerator(rescale = 1. / 255)
-
-  train_gen = train_datagen.flow_from_directory(
-    train_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    color_mode = 'grayscale',
-    class_mode = 'categorical'
-  )
-
-  test_gen = test_datagen.flow_from_directory(
-    test_dir,
-    target_size = (img_width, img_height),
-    batch_size = batch_size,
-    color_mode = 'grayscale',
-    class_mode = 'categorical'
-  )
-
   return (nb_classes, nb_train_samples, nb_test_samples, nb_channels, train_dir, test_dir)
 
