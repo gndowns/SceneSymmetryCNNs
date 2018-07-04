2677b59700e4a9ec27fb77047475b977dc283f83
Gabriel Downs <gndowns@gmail.com>
2018-07-04 01:26:12 -0400

    train bottleneck features of each fold

--- k_fold_cv/k_fold.py
+++ k_fold_cv/k_fold.py
@@ -2,22 +2,22 @@
 # First tunes top dense layers, then 
 # top layers with convolutional layers all together
 
-# NOTE: for now, just try with tiny_cnn, compare with old results
 
-from k_fold_dataset.k_fold_dataset import KFoldDataset
-import load_models
+from k_fold_dataset import KFoldDataset
+#  import load_models
 from sklearn.model_selection import StratifiedKFold
 from keras import backend as K
-
 import numpy as np
+
+from top_model import bottleneck_features, train_top_model
 
 # global training params
 EPOCHS = 10
 BATCH_SIZE = 16
-IMG_SIZE = (256,256)
-COLOR_MODE = 'grayscale'
-# 1 channel for grayscale
-INPUT_SHAPE = IMG_SIZE + (1,)
+IMG_SIZE = (224,224)
+COLOR_MODE = 'rgb'
+# 3 channels for VGG compatability 
+INPUT_SHAPE = IMG_SIZE + (3,)
 
 # train and evaluate model
 def train_fold(dataset, X, Y, train_index, test_index):
@@ -49,18 +49,26 @@ def k_fold_cross_val(dataset):
   # get class labels from one-hot encodings (required for sklearn kfold method)
   labels = [np.argmax(y) for y in Y]
 
+  # generate bottleneck features (output of conv layers of VGG16)
+  X_bottleneck = bottleneck_features(X) 
+
 
   scores = []
+  i=1
   # train a model for each fold
-  for train_index, test_index in kfold.split(X,labels):
-    #  top_model = top_model_k_fold
-    score = train_fold(dataset,X, Y, train_index, test_index)
-    scores.append(score)
+  for train_idx, test_idx in kfold.split(X,labels):
+    print('fold ' + str(i) + ' of 5')
+
+    # train top model on bottleneck features
+    top_model_weights = train_top_model(
+      X_bottleneck[train_idx], Y[train_idx],
+      X_bottleneck[test_idx], Y[test_idx]
+    )
 
     # clear memory
     K.clear_session()
-    #  del model
-    #  gc.collect()
+
+    i += 1
 
   print(scores)
   print(np.mean(scores))
---
+++ k_fold_cv/top_model.py
@@ -0,0 +1,64 @@
+# Trains the top dense layers of VGG16 for k-fold cross validation datasets
+# Note this file differes from the root level `top_model.py`
+# Each train/test split must be trained on both this top_model 
+# AND the lower convolutional layers,
+# so this file is not meant to be run independently, 
+# but rather called repeatedly as part of the entire `k_fold.py` routine
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import VGG16
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Dropout
+from keras.optimizers import RMSprop
+
+# global training params
+#  EPOCHS = 100
+# fewer epochs needed for Toronto 475
+EPOCHS = 50
+BATCH_SIZE = 16
+
+
+# Run convolution layers of VGG16 to produce output 'bottleneck feature" maps
+# these will be used as input for our Top Dense Model
+# since this file is used for k_fold cross-validation datasets, there is no 
+# distinction between train and test features
+# INPUT: X, a numpy array of all images in dataset
+def bottleneck_features(X):
+  # import pre-trained VGG16 WITHOUT top dense layers
+  vgg = VGG16(include_top=False, weights='imagenet')
+
+  # get feature maps as output from conv layers of pre-trained VGG16
+  print('Calculating bottleneck features of data...')
+  features = vgg.predict(X, verbose=1)
+
+  return features
+
+
+# trains top model on bottleneck features and returns weights
+def train_top_model(x_train, y_train, x_test, y_test):
+  # load top model architecture
+  model = Sequential()
+  # use shape of data for input shape
+  model.add(Flatten(input_shape=x_train.shape[1:]))
+  model.add(Dense(256, activation='relu'))
+  model.add(Dropout(0.5))
+  # use shape of labels to get number of classes
+  model.add(Dense(y_train.shape[1], activation='softmax'))
+
+  model.compile(
+    # lower learning rate for fine tuning
+    optimizer=RMSprop(lr=1e-5),
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+  )
+
+  # train the model
+  model.fit(x_train, y_train,
+    epochs = EPOCHS,
+    batch_size = BATCH_SIZE,
+    validation_data = (x_test, y_test)
+  )
+
+  # return the trained weights
+  return model.get_weights()
+
