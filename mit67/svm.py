f74c0b241c1c99be80b4c40939b2d204c8ef7e74
Gabriel Downs <gndowns@gmail.com>
2018-11-05 08:52:34 -0500

    use pre-trained vgg11 in mit67 svm test

--- mit67/svm.py
+++ mit67/svm.py
@@ -1,6 +1,6 @@
 # Train a linear SVM classifier on the deep features output by Vgg16_hybrid
 
-from vgg16_utils import vgg16_hybrid_1365
+from vgg16_utils import vgg16_hybrid_1365, vgg11
 from mit67_dataset import MIT67Dataset
 from thundersvmScikit import SVC
 import numpy as np
@@ -11,19 +11,29 @@ def train_and_test(datasets):
   # use first listed dataset for training
   train_dataset = datasets[0]
 
-  model = vgg16_hybrid_1365(1)
+  # (using original rgb weights)
+  #  model = vgg16_hybrid_1365(1)
+  #  color_mode = 'rgb'
+
+  # using line drawing places weights
+  model = vgg11(365, 1)
+  weights_file = 'models/places365_vgg11_runaway_weights.h5'
+  model.load_weights(weights_file)
+  model.pop()
+  color_mode = 'grayscale'
+
 
   # standards
   img_size = (224,224)
-  color_mode = 'rgb'
 
   # use rescale=1 to match places conventions
   x_train, y_train = train_dataset.train_data(img_size, color_mode, 1)
   x_test, y_test = train_dataset.test_data(img_size, color_mode, 1)
 
   # generate bottleneck features (output of conv layers)
-  bneck_train = model.predict(x_train)
-  bneck_test = model.predict(x_test)
+  print('generating bottleneck features...')
+  bneck_train = model.predict(x_train, verbose=1)
+  bneck_test = model.predict(x_test, verbose=1)
 
   # convert one-hot encodings to class labels
   train_labels = [np.argmax(y) for y in y_train]
@@ -31,14 +41,16 @@ def train_and_test(datasets):
 
   # train svm
   svc = SVC(kernel='linear')
+  print('training svm...')
   svc.fit(bneck_train, train_labels)
 
+  print('scoring...')
   score = svc.score(bneck_test, test_labels)
 
   print(score)
 
   # Different trials
-  print('training on other datasets...')
+  print('testing on other datasets...')
   for dataset in datasets[1:]:
     print(dataset.str)
     x_test, y_test = dataset.test_data(img_size, color_mode, 1)
@@ -57,7 +69,8 @@ def main():
   # the first listed will be used for training,
   # all others will be tested on
   #  dataset_strs = ['line_drawings', 'dR_symmetric', 'dR_asymmetric']
-  dataset_strs = ['smooth', 'smooth_dR_symmetric', 'smooth_dR_asymmetric']
+  dataset_strs = ['smooth']
+  #  dataset_strs = ['smooth', 'smooth_dR_symmetric', 'smooth_dR_asymmetric']
   #  dataset_strs = ['rgb']
 
   datasets = [MIT67Dataset(s) for s in dataset_strs]
